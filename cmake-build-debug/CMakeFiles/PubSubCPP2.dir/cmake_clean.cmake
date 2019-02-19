a8848b7bfa7c15c3c05c54a12594f992e64180d6
maksutovaynur <maksutov_aynur@mail.ru>
2019-02-19 12:22:17 +0300

    FIRST

---
+++ cmake-build-debug/CMakeFiles/PubSubCPP2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PubSubCPP2.dir/main.cpp.o"
+  "PubSubCPP2.pdb"
+  "PubSubCPP2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PubSubCPP2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
