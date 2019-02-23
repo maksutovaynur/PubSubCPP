dc15104cfc0105cd7c1ef4087fb8ff0f36230b7b
maksutovaynur <maksutov_aynur@mail.ru>
2019-02-23 17:51:11 +0300

    OK

---
+++ cmake-build-debug/CMakeFiles/measure.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/measure.dir/measure.cpp.o"
+  "measure.pdb"
+  "measure"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/measure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/pubsub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pubsub.dir/pubsub.cpp.o"
+  "pubsub.pdb"
+  "pubsub"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pubsub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
