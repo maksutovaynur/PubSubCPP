832ce351e3ff4492d5e8724093fe62543a97e2cc
maksutovaynur <maksutov_aynur@mail.ru>
2019-02-23 11:44:00 +0300

    Make some tests

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -46,5 +46,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/pubsub.dir/DependInfo.cmake"
+  "CMakeFiles/measure.dir/DependInfo.cmake"
   "CMakeFiles/rmtopic.dir/DependInfo.cmake"
   )
--- topic.hpp
+++ topic.hpp
@@ -271,32 +271,32 @@ public:
         return tpc::interrupted;
     }
 
-    using Top = std::unique_ptr<Topic>;
+    using TopPtr = std::unique_ptr<Topic>;
 
     static bool remove(const std::string &name) {
         auto t = std::make_unique<Topic>(name, 0, 0);
         return t->remove();
     }
 
-    static Top spawn(const std::string &name, ui msg_size, ui msg_count) {
+    static TopPtr spawn(const std::string &name, ui msg_size, ui msg_count) {
         auto t = std::make_unique<Topic>(name, msg_size, msg_count);
         if (t->start(false, false, false)) return t;
         else return nullptr;
     }
 
-    static Top spawn(const std::string &name, ui msg_size) {
+    static TopPtr spawn(const std::string &name, ui msg_size) {
         auto t = std::make_unique<Topic>(name, msg_size, 0);
         if (t->start(false, false, true)) return t;
         else return nullptr;
     }
 
-    static Top spawn_create(const std::string &name, ui msg_size, ui msg_count) {
+    static TopPtr spawn_create(const std::string &name, ui msg_size, ui msg_count) {
         auto t = std::make_unique<Topic>(name, msg_size, msg_count);
         if (t->start(true, false, false)) return t;
         else return nullptr;
     }
 
-    static Top spawn(const std::string &name) {
+    static TopPtr spawn(const std::string &name) {
         auto t = std::make_unique<Topic>(name, 0, 0);
         if (t == nullptr) return nullptr;
         if (t->start(false, true, true)) return t;
